264182736154f01b7f60a100e4ca1a292cb62d63
unknown <lucas.rutowski@gmail.com>
2020-10-15 10:41:35 -0500

    added game, player (human and AI), and gesture classes

---
+++ RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock/Gesture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsLizardSpock
+{
+    class Gesture
+    {
+        string gestureType;
+
+        public Gesture(String gestureType)
+        {
+            this.gestureType = gestureType;
+        }
+    }
+}
---
+++ RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsLizardSpock
+{
+    class Player
+    {
+        public Gesture gesture;
+        public string name;
+
+        public Player()
+        {
+
+        }
+
+        public virtual Gesture ChooseGesture(List<Gesture> gestures)
+        {
+            // choose gesture codeblock
+        }
+    } 
+}
